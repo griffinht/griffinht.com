053a942a2815b072caf37750350fb627e5456972
stzups <griffinht@gmail.com>
2021-12-20 15:39:58 -0500

    multi page build

---
+++ build.sh
@@ -0,0 +1,15 @@
+rm -rf build/*
+mkdir -p build
+for file in ./src/*.html; do
+    html-minifier \
+        --collapse-whitespace \
+        --remove-comments \
+        --remove-optional-tags \
+        --remove-redundant-attributes \
+        --remove-script-type-attributes \
+        --remove-tag-whitespace \
+        --use-short-doctype \
+        --minify-css true \
+        --minify-js true \
+        < "$file" > build/"$(basename $file)"
+done
