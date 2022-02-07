1a0e8ecf64fd247c0e33ca2f30b6fe832e45e9f7
stzups <griffinht@gmail.com>
2022-02-07 13:44:53 -0500

    fix math

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ BUILD=build
 
 build() {
     file="$1"
-    short=$(echo "$file" | cut -c ${#SOURCE}-);
+    short=$(echo "$file" | cut -c $((${#SOURCE}+1))-);
     if [ -d "$file" ]; then
         mkdir -p "$BUILD""$short"
     else
@@ -22,7 +22,7 @@ build() {
 }
 
 if [ "$1" == "--watch" ]; then
-    inotifywait -r -m -e close_write --format %w%f "$SOURCE" | 
+    inotifywait -r -m -e close_write --format %f "$SOURCE" | 
         while read -r file; 
             do ./build.sh "$file"; 
         done
