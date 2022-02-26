b8acdd9006f63659f44d78730ab4ecd83ec011bb
stzups <griffinht@gmail.com>
2022-02-26 17:34:08 -0500

    start to add getopt

--- scripts/build.py
+++ scripts/build.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
 import subprocess
 import importlib
+import getopt
 
-SOURCE="src"
-BUILD="build"
+NAME="build"
+VERSION="0.0.1"
+AUTHOR="griffinht"
+DEFAULT_INPUT=""
+DEFAULT_OUTPUT="build"
 
 """
 def build():
@@ -36,38 +40,69 @@ def build():
 """
 
 
-def build(file):
-  print("build: " + file)
-  if (file.endswith(os.path.sep)):
-    os.makedirs(file, exist_ok=True)
+def build(input, file, output):
+  print(file + ": ", end = "")
+  if (os.path.isdir(input + os.path.sep + file)):
+    print("making directories")
+    os.makedirs(input + os.path.sep + file, exist_ok=True)
   else:
-    print(file)
+    print("building file")
 
 def watch(directory):
   print("import pyinotify")
   pyinotify = importlib.import_module("pyinotify")
 
   def update(update):
-    print("update")
-    print(update.mask)
     sys.stdout.flush()
     build(update.path + update.name)
 
   wm = pyinotify.WatchManager()
-  wm.add_watch(directory, pyinotify.IN_CLOSE_WRITE | pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY, update)
+  wm.add_watch(directory, pyinotify.IN_CLOSE_WRITE | pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY, update, rec=True)
   print("looping")
   sys.stdout.flush()
   notifier = pyinotify.Notifier(wm)
   notifier.loop()
 
-def buildDirectory(directory):
-  for path, directories, files in os.walk(directory):
+def buildDirectory(input, output):
+  for path, directories, files in os.walk(input):
     for directory in directories:
-      build(path + os.path.sep + directory)
+      build(path, directory, output)
     for file in files:
-      build(path + os.path.sep + file)
+      build(input, file, output)
 
 def init():
+  input = DEFAULT_INPUT
+  output = DEFAULT_OUTPUT
+  watch = False
+
+  try:
+    options, args = getopt.gnu_getopt(sys.argv[1:], "hvow", ["help", "version", "output", "watch"])
+  except getopt.GetoptError as e:
+    print(e)
+    usage()
+    sys.exit(2)
+
+  for option, value in options:
+    if option in ("-v", "--version"):
+      print(NAME + " v" + VERSION + " by " + AUTHOR)
+      sys.exit(0)
+    if option in ("-h", "--help"):
+      usage()
+      sys.exit(0)
+    elif option in ("-o", "--output"):
+      print("output")
+    elif option in ("-w", "--watch"):
+      print("watch")
+    else:
+      print("unhandled")
+    sys.exit(0)
+  if (len(args) > 0):
+    input = args[0]
+    if (len(args) > 1):
+      print("warning: ignoring extraneous trailing arguments: " + str(args[1:]))
+
+  print(input + ", " + output + ", " + str(watch))
+
   if (len(sys.argv) > 1):
     if (sys.argv[1] == "--watch"):
       directory = ""
@@ -77,9 +112,9 @@ def init():
       watch(directory)
     else:
       print("building " + sys.argv[1])
-      buildDirectory(sys.argv[1])
+      #buildDirectory(sys.argv[1])
   else:
     print("building " + os.getcwd())
-    buildDirectory("")
+    #buildDirectory("")
 
 init()
