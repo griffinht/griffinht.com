93f59dc61647c39a95e46340ae5bddae08111e95
stzups <griffinht@gmail.com>
2022-02-27 11:11:04 -0500

    remove verbose

--- scripts/build.py
+++ scripts/build.py
@@ -15,7 +15,7 @@ AUTHOR="griffinht"
 DEFAULT_INPUT=""
 DEFAULT_OUTPUT=""
 
-def build_template(verbose, input_path, template_path, output_path):
+def build_template(input_path, template_path, output_path):
   print(input_path + " + "  + template_path + " -> " + output_path)
   with open(template_path, "r") as stream:
     try:
@@ -27,7 +27,7 @@ def build_template(verbose, input_path, template_path, output_path):
       print(e)
       return
 
-def build_file(verbose, copy, input_path, output_path):
+def build_file(copy, input_path, output_path):
   if copy:
     shutil.copyfile(input_path, output_path)
   else:
@@ -38,7 +38,7 @@ def build_file(verbose, copy, input_path, output_path):
     except Exception as e:
       raise e
 
-def watch(verbose, copy, input, output):
+def watch(copy, input, output):
   print("import asyncinotify")
   asyncinotify = importlib.import_module("asyncinotify")
   asyncio = importlib.import_module("asyncio")
@@ -72,7 +72,9 @@ def watch(verbose, copy, input, output):
       loop.run_until_complete(loop.shutdown_asyncgens())
       loop.close()
 
-def build_directory(verbose, copy, input, output):
+def build():
+
+def build_directory(copy, input, output):
   length = len(input)
   for path, directories, files in os.walk(input):
     post_path  = path[length:]
@@ -89,24 +91,22 @@ def build_directory(verbose, copy, input, output):
       if extension[-1] == "yml":
         for f in files:
           if f.split(".")[0] == extension[0]:
-            build_template(verbose, path + os.path.sep + f, input_path, output + post_path + os.path.sep + f)
+            build_template(path + os.path.sep + f, input_path, output + post_path + os.path.sep + f)
             break
       elif not extension[-1] == "mustache":
-        build_file(verbose, copy, input_path, output_path)
+        build_file(copy, input_path, output_path)
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-v", "--version", action="version", version=NAME + " v" + VERSION + " by " + AUTHOR)
   parser.add_argument("-o", "--output", default=DEFAULT_OUTPUT)
   parser.add_argument("-w", "--watch", default=False, action='store_true')
-  parser.add_argument("-q", "--quiet", default=False, action='store_true')
   parser.add_argument("-c", "--copy", default=False, action='store_true')#todo description
   parser.add_argument("REMAINDER", nargs=argparse.REMAINDER)
   parsed = parser.parse_args(sys.argv[1:])
 
   output = parsed.output
   isWatch = parsed.watch
-  verbose = not parsed.quiet
   copy = parsed.copy
   length = len(parsed.REMAINDER)
   if length == 0:
@@ -118,10 +118,10 @@ def main():
 
   if isWatch:
     print("watching " + os.getcwd() + os.path.sep + input)
-    watch(verbose, copy, input, output)
+    watch(copy, input, output)
   else:
     print("building " + os.getcwd() + os.path.sep + input)
-    build_directory(verbose, copy, input, output)
+    build_directory(copy, input, output)
 
 if __name__ == '__main__':
     main()
